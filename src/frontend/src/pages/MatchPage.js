4b30301f041c463b04284549256ef95eaa0c6c22
arijit-mukherjee24 <arijitcode10@gmail.com>
2021-05-26 18:18:38 +0530

    Completed UI elements for Team Page

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MatchPage = () => {
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+        </div>
+    );
+}
+
+// url - http://localhost:3000/teams/Sunrisers%20Hyderabad/matches/2019
